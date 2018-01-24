e39a91c7db26092b0de403af11437ee5bdf7d3e9
akaashv92 <akaashv92@gmail.com>
2018-01-24 17:29:24 -0500

    Initial commit with socket IO

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const path = require('path');
-
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 
 const publicPath = path.join(__dirname,'../public');
 const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user conencted');
+
+  socket.on('disconnect', () => {
+    console.log('disconnected from server (server.js)');
+  });
+});
 app.use(express.static(publicPath));
 
 // app.get('/', (req,res) => {
 //   res.send()
 // })
 
-app.listen(PORT, () => {
-  console.log('Server running on port 3000');
+server.listen(PORT, () => {
+  console.log('Server running on port : ' +PORT);
 })
