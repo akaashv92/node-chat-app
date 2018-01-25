80e181cc460eab5aa7cd1e8db0f690b3ec5557e4
akaashv92 <akaashv92@gmail.com>
2018-01-25 13:33:49 -0500

    Added form for messages and jquery

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,26 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage' , function (newMessage) {
   console.log('New Message', newMessage);
+  var li = $('<li></li>');
+
+  li.text(`${newMessage.from} : ${newMessage.text}`);
+  $("#messages").append(li);
+});
+
+// socket.emit('createMessage', {
+//   from : 'frank',
+//   text :'Hi'
+// }, function (data) {
+//   console.log('Got it', data);
+// });
+
+$('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from : 'User',
+    text : $('[name=message]').val(),
+  }, function(data) {
+
+  });
 });
