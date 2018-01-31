2342e07d9e1332fa3399f5d5668ae421cc5ccea0
akaashv92 <akaashv92@gmail.com>
2018-01-31 12:49:39 -0500

    Added auto scrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom (){
+  var message = $('#messages');
+  var newMessage = message.children('li:last-child');
+  var clientHeight = message.prop('clientHeight');
+  var scrollTop = message.prop('scrollTop');
+  var scrollHeight = message.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    message.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect' , function() {
   console.log('Connected to server');
 });
@@ -18,6 +32,7 @@ socket.on('newMessage' , function (message) {
     createdAt: formattedTime
   });
   $("#messages").append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
