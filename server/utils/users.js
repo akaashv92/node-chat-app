e0d46e2d804162f1e0185f19eae5b919dd953ef2
akaashv92 <akaashv92@gmail.com>
2018-02-05 14:02:29 -0500

    removed debuggers

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,18 +1,3 @@
-class Person {
-  constructor(name, age){
-      this.name = name;
-      this.age = age;
-  }
-
-  getUserDescription(){
-    return `${this.name} is ${this.age} year(s) old`;
-  }
-}
-
-var me = new Person('akaash', 25);
-var desc = me.getUserDescription();
-console.log(desc);
-
 class Users {
   constructor(){
     this.users = [];
